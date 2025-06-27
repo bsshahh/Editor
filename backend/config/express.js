b909eb5f3b072bb6cab0713f736ace9e7e020ae5
bsshahh <shahbhavya338@gmail.com>
2025-06-27 15:08:48 +0530

    midchange

--- backend/config/express.js
+++ backend/config/express.js
@@ -3,7 +3,11 @@ import express from "express";
 import setupMiddleware from "../middleware/SetUpmiddleware.js";
 import routes from "../routes/routes.js";
 const app=express();
+const configureApp = async () => {
+  await setupMiddleware(app); 
+  routes(app);                
+};
 
-setupMiddleware(app);
-routes(app);
+await configureApp(); 
+
 export default app;
