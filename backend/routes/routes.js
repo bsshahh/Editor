f74b3f7baf5f39224fc28652d0dbfe36aa7c93b5
bsshahh <shahbhavya338@gmail.com>
2025-06-27 12:56:32 +0530

    Refactor routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,6 +4,9 @@ import auth from "./auth.js";
 import admintest from "./admintest.js";
 import usertest from "./usertest.js";
 const routes=(app)=>{
+    app.get("/", (req, res) => {
+    res.status(200).send("✅ Backend is live and healthy!");
+  });
     app.use("/api/code",runcode);
     app.use("/api/user",userreg);
     app.use("/api/auth",auth);
